dba6fd68b844154326d65f2353528a279e4f4143
courtney long <courtneylong877@gmail.com>
2021-09-23 12:33:46 -0400

    works super well, needs styling and log high score with initials

--- thisshouldwork.js
+++ thisshouldwork.js
@@ -28,45 +28,176 @@ const wrongA6 = document.querySelectorAll(".wrong6");
 // Variables for the starting elements and the final page.
 
 var firstPage = document.querySelector("header");
-var startButton = document.getElementById("start");
-var lastPage = document.querySelector("Results");
-
+var startButton = document.querySelector(".start");
+var lastPage = document.querySelector(".Results");
 
 // Variables for HTML placeholders.
 
 var amIRight = document.getElementById("RightorWrong");
-var timer = document.getElementById("countdown");
+var lastQuestionResult = document.getElementById("finalQuestion");
+var clock = document.getElementById("countdown");
+var time = 90;
+var remainingTime = document.getElementById("score");
+
+// Hide all HTML but the header.
+
+question1.style.display = "none";
+question2.style.display = "none";
+question3.style.display = "none";
+question4.style.display = "none";
+question5.style.display = "none";
+question6.style.display = "none";
+lastPage.style.display = "none";
+
+// Adding variable for the countdown function.
+
+function runTheClock() {setInterval(function() {
+    time--;
+    clock.textContent = time + " seconds left";
+}, 1000);
+};
+
+// Adding a variable for functions to display if answer is right or wrong.
+
+var displayRightResult = setTimeout(function() {
+    amIRight.innerHTML = "Right!";
+}, 2000);
+
+var displayWrongResult = setTimeout(function() {
+    amIRight.innerHTML = "Wrong!";
+}, 2000);
+
+// Function for clicking start button.
+
+var beginTheQuiz = function (){
+    runTheClock();
+    firstPage.style.display = "none";
+    question1.style.display = "block";
+};
+
+// Displaying question 2 and showing right or wrong answer results.
+
+var question1Right = function () {
+    question1.style.display = "none";
+    question2.style.display = "block";
+    displayRightResult;
+};
+
+var question1Wrong = function () {
+    question1.style.display = "none";
+    question2.style.display = "block";
+    time-=5;
+    displayWrongResult;
+};
+
+// Displaying question 3 and showing right or wrong answer results.
+
+var question2Right = function () {
+    question2.style.display = "none";
+    question3.style.display = "block";
+    displayRightResult;
+};
+
+var question2Wrong = function () {
+    question2.style.display = "none";
+    question3.style.display = "block";
+    time-=5;
+    displayWrongResult;
+};
+
+// Displaying question 4 and showing right or wrong answer results.
+
+var question3Right = function () {
+    question3.style.display = "none";
+    question4.style.display = "block";
+    displayRightResult;
+};
+
+var question3Wrong = function () {
+    question3.style.display = "none";
+    question4.style.display = "block";
+    time-=5;
+    displayWrongResult;
+};
+
+// Displaying question 5 and showing right or wrong answer results.
+
+var question4Right = function () {
+    question4.style.display = "none";
+    question5.style.display = "block";
+    displayRightResult;
+};
+
+var question4Wrong = function () {
+    question4.style.display = "none";
+    question5.style.display = "block";
+    time-=5;
+    displayWrongResult;
+};
+
+// Displaying question 6 and showing right or wrong answer results.
+
+var question5Right = function () {
+    question5.style.display = "none";
+    question6.style.display = "block";
+    displayRightResult;
+};
+
+var question5Wrong = function () {
+    question5.style.display = "none";
+    question6.style.display = "block";
+    time-=5;
+    displayWrongResult;
+};
+
+// Hiding the remaining questions and displaying the final page.
+
+var question6Right = function () {
+    question6.style.display = "none";
+    lastPage.style.display = "block";
+    lastQuestionResult.textContent = "Right!"
+};
+
+var question6Wrong = function () {
+    question6.style.display = "none";
+    lastPage.style.display = "block";
+    lastQuestionResult.textContent = "Wrong!"
+    time-=5;
+    clock.style.display = "none";
+    var finalScore=time;
+    remainingTime.textContent = finalScore;
+};
 
 // Adding event listeners to the "wrong answer" buttons.
 
-buttons.forEach(wrongA1 => {
-    wrongA1.addEventListener("click",);
+wrongA1.forEach(wrongA1 => {
+    wrongA1.addEventListener("click", question1Wrong);
 })
-buttons.forEach(wrongA2 => {
-    wrongA2.addEventListener("click",);
+wrongA2.forEach(wrongA2 => {
+    wrongA2.addEventListener("click", question2Wrong);
 })
-buttons.forEach(wrongA3 => {
-    wrongA3.addEventListener("click",);
+wrongA3.forEach(wrongA3 => {
+    wrongA3.addEventListener("click", question3Wrong);
 })
-buttons.forEach(wrongA4 => {
-    wrongA4.addEventListener("click",);
+wrongA4.forEach(wrongA4 => {
+    wrongA4.addEventListener("click", question4Wrong);
 })
-buttons.forEach(wrongA5 => {
-    wrongA5.addEventListener("click",);
+wrongA5.forEach(wrongA5 => {
+    wrongA5.addEventListener("click", question5Wrong);
 })
-buttons.forEach(wrongA6 => {
-    wrongA6.addEventListener("click",);
+wrongA6.forEach(wrongA6 => {
+    wrongA6.addEventListener("click", question6Wrong);
 })
 
 // Adding event listeners to the "right answer" buttons.
 
-correctA1.addEventListener("click",);
-correctA2.addEventListener("click",);
-correctA3.addEventListener("click",);
-correctA4.addEventListener("click",);
-correctA5.addEventListener("click",);
-correctA6.addEventListener("click",);
+correctA1.addEventListener("click", question1Right);
+correctA2.addEventListener("click", question2Right);
+correctA3.addEventListener("click", question3Right);
+correctA4.addEventListener("click", question4Right);
+correctA5.addEventListener("click", question5Right);
+correctA6.addEventListener("click", question6Right);
 
 // Adding an event listener to the start button.
 
-startButton.addEventListener("click",);
+startButton.addEventListener("click", beginTheQuiz);
